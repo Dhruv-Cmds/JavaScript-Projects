07cee0ba0879eb09bfbcd416943625d9408767f7
Dhruv-Cmds <pd1900565@gmail.com>
2026-08-04 10:26:06 +0530

    add day 1 backend

---
+++ JavaScript-backend/server.js
@@ -0,0 +1,14 @@
+const http = require('node:http');
+
+// const hostname = "127.0.0.1"
+// const port = 3000;
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Hello world\n");
+});
+
+server.listen(3000, "127.0.0.1", () => {
+    console.log(`Server running at http://${"127.0.0.1"}:${3000}/`);    
+});
